e2a10c834758f8ee8c0a9b72cc2b1161b2a8f1ec
Matthew Ames <maverickames@gmail.com>
2024-03-14 08:52:34 -0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maverickames/telegramBot
+
+go 1.18
---
+++ telegram.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func sendTelegramBotNotification(message string) error {
+	botToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if botToken == "" {
+		return fmt.Errorf("TELEGRAM_BOT_TOKEN env not set")
+	}
+
+	chatID := os.Getenv("TELEGRAM_CHAT_ID")
+	if chatID == "" {
+		return fmt.Errorf("TELEGRAM_CHAT_ID env not set")
+	}
+
+	endpoint := fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", botToken)
+	response, err := http.PostForm(endpoint, url.Values{"chat_id": {chatID}, "text": {message}})
+	if err != nil {
+		return err
+	}
+	if response.StatusCode != 200 {
+		return fmt.Errorf("telegram response status Code: %d", response.StatusCode)
+	}
+
+	// defer response.Body.Close()
+	// body, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	return err
+	// }
+	// fmt.Println(string(body))
+	return nil
+}
